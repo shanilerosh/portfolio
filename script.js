f168964c8c5bc869b4944cfad46d47078b6a05df
Shanil Miranda <shanilmiranda24@gmail.com>
2020-12-21 08:01:45 +0530

    added the spinner

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("body").style.visibility = "visible";
+    document.querySelector("#loader").style.visibility = "hidden";
+  }
+};
+
 const hamburger = document.getElementById("hamburger");
 const navUl = document.getElementById("list");
 
